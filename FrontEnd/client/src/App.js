5ed925d812f8cba2d75ae9c045f182faf3d31410
madrackwp <52032916+madrackwp@users.noreply.github.com>
2022-05-06 13:18:43 +0800

    Creating routes

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-<<<<<<< HEAD
 import ExpensesPage from "./Pages/Expenses";
 
 function App() {
@@ -11,18 +10,6 @@ function App() {
       </Routes>
     </div>
   );
-=======
-import LoginPage from "./Pages/LoginPage";
-import { Route} from 'react-router-dom'
-
-function App() {
-  return (
-  //<div className="App">Hello world</div>;
-  <Route exact path="/">
-  <LoginPage />
-</Route>
-  )
->>>>>>> b1840f476bcfb9b21fd6cbe1295f5b60d8e4902e
 }
 
 export default App;
