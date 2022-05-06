b1840f476bcfb9b21fd6cbe1295f5b60d8e4902e
cyberbeann <s.wruiqi@gmail.com>
2022-05-06 13:13:12 +0800

    Adding login page

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import LoginPage from "./Pages/LoginPage";
+import { Route} from 'react-router-dom'
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+  //<div className="App">Hello world</div>;
+  <Route exact path="/">
+  <LoginPage />
+</Route>
+  )
 }
 
 export default App;
