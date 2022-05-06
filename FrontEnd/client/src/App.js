d60d79e754dffb69ec0f0de9fc434789b4c146ad
cyberbeann <s.wruiqi@gmail.com>
2022-05-06 16:56:33 +0800

    GGWP

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,8 +1,3 @@
-import React from "react";
-import "./App.css";
-import Button from "./Pages/Button";
-import DashboardPage from "./Pages/DashboardPage";
-import AddProject from "./Pages/AddProject";
 
 const App = () => {
   return (
@@ -15,4 +10,25 @@ const App = () => {
     </div>
   );
 };
+
+import { Route, Routes } from "react-router-dom";
+//import "./App.css";
+import ExpensesPage from "./Pages/Expenses";
+import LoginPage from "./Pages/LoginPage";
+import Button from "./Pages/Button";
+import DashboardPage from "./Pages/DashboardPage";
+import AddProject from "./Pages/AddProject";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/expenses" element={<ExpensesPage />} />
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+
+    </div>
+  );
+}
+
 export default App;
