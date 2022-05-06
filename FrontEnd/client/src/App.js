1fd18b45946edaa83b58712f1f5763ab752124b8
madrackwp <52032916+madrackwp@users.noreply.github.com>
2022-05-06 13:17:11 +0800

    creating routes

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ExpensesPage from "./Pages/Expenses";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/expenses" element={<ExpensesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ FrontEnd/client/src/Pages/Expenses.js
@@ -0,0 +1,5 @@
+function ExpensesPage() {
+  return <div>HELLO WORLD THIS IS EXPENSES PAGE</div>;
+}
+
+export default ExpensesPage;
