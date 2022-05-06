e064b42ee56c36f1e2c83d90a909262d26f37279
madrackwp <52032916+madrackwp@users.noreply.github.com>
2022-05-06 16:05:46 +0800

    Added the cards with the category dropdown

---
+++ FrontEnd/client/src/components/NavigationBar.js
@@ -0,0 +1,12 @@
+import classes from "./NavigationBar.module.css";
+
+function NavigationBar() {
+  return (
+    <header className={classes.navbar}>
+      <p>Group 3</p>
+      <button>Logout</button>
+    </header>
+  );
+}
+
+export default NavigationBar;
